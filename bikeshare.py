f2da8d8e3bcf8ac5c163279b5d38e2756d3087ad
LoiNLT <loinguyenlamthanh@gmail.com>
2024-09-05 16:07:03 +0700

    refactor: add comment for file bikeshare

--- bikeshare.py
+++ bikeshare.py
@@ -1,6 +1,7 @@
 import time
 import pandas as pd
 
+# add comment for file
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
